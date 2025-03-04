0339634be7c2ce8467bc2d90af0861b606f196b6
Fernando Garrido <siberiancoffe@outlook.cl>
2025-03-04 14:07:19 -0300

    Refactor configuration and main logic to support multiple messages and reduce delay

--- main.py
+++ main.py
@@ -8,33 +8,46 @@ def openConfig():
         data = loads(json_file.read())
         json_file.close()
         return data
-
-def process(msj: str, time: int):
-    while True:
-        sleep(time*60)
-        try:
-            press("y")
-            copy(msj)
-            hotkey('ctrl', 'v')
-            press("enter")
-        except:
-            break
-
-def main():
-    configs = openConfig()
-    print(f'''
+    
+def printConfig(msj1: str, msj2: str, time: int):
+        print(f'''
 *************************
 Simple SPAM BOT
 By: SiberianCoffe
 https://github.com/CoffeSiberian
 *************************
 
-Message to send: {configs["msj"]}
+Message to send 1: {msj1}
+Message to send 2: {msj2}
 
-Each {configs["delay"]} minutes
+Each {time} minutes
     ''')
 
-    process(configs["msj"], configs["delay"])
+def process(msj1: str, msj2: str, time: int):
+    second_active_message = False
+
+    while True:
+        if second_active_message:
+            copy(msj2)
+            second_active_message = False
+        else:
+            copy(msj1)
+            second_active_message = True
+
+        sleep(time*60)
+        try:
+            press("y", interval=0.5)
+            hotkey('ctrl', 'v', interval=0.5)
+            press("enter", interval=0.5)
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            break
+
+def main():
+    configs = openConfig()
+
+    printConfig(configs["msj1"], configs["msj2"], configs["delay"])
+    process(configs["msj1"], configs["msj2"], configs["delay"])
 
 if __name__ == "__main__":
     try:
